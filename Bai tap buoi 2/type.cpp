45efb21952df39f6be26ea83e07c1a0c20b74ee5
pdhung1992 <115760562+pdhung1992@users.noreply.github.com>
2022-10-14 10:31:17 +0700

    print

---
+++ Bai tap buoi 2/type.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int x = 10;
+	float y = 3.14;
+	double z = -15.0023;
+	char h = 'A';
+	
+	printf("x = %d", x);
+	printf("y = %f", y);
+	printf("z = %lf",z);
+	printf("h = %c", h);
+}
