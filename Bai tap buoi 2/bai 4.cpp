0375753a249b303a9766e26a362b987e5691938f
pdhung1992 <115760562+pdhung1992@users.noreply.github.com>
2022-10-18 14:30:28 +0700

    Update bai 4.cpp

--- Bai tap buoi 2/bai 4.cpp
+++ Bai tap buoi 2/bai 4.cpp
@@ -2,6 +2,7 @@
 #include <math.h>
 int main()
 {
+
 	printf("nhap vao 3 so a, b, c:\n");
 	
 	int a;
@@ -12,21 +13,30 @@ int main()
 	scanf("%d", &b);
 	scanf("%d", &c);
 	
-	int d = b*b-4*a*c;
-	
-	if(d<0){
-		printf("phuong trinh vo nghiem!");
-	}else{
-		if(d==0){
-			int x = -b/(2*a);
-			printf("phuong trinh co nghiem kep: %d", x);
+	if(a==0){
+		if(b==0){
+			if(c==0){
+				printf("phuong trinh vo so nghiem");
+			}else{
+				printf("phuong trinh vo nghiem");
+			}
 		}else{
-			float x1 = (-b+sqrt(d))/(2*a);
-			float x2 = (-b-sqrt(d))/(2*a);
-			printf("phuong trinh co 2 nghiem don:\n %f\n %f", x1, x2);
+			float x = -(float)c / b;
+			printf("phuong trinh co 1 nghiem:\n x= %f", x);
+		}
+	}else{
+		int d = b*b-4*a*c;
+		if(d<0){
+			printf("phuong trinh vo nghiem");
+		}else{
+			if(d==0){
+				float x = -(float)b/(2*a);
+				printf("phuong trinh co nghiem kep:\n x= %f", x);
+			}else{
+				double x1 = (-b+sqrt(d))/(2*a);
+				double x2 = (-b-sqrt(d))/(2*a);
+				printf("phuong trinh co 2 nghiem don:\n %lf\n %lf", x1, x2);
+			}
 		}
 	}
-	
-	
 }
-
